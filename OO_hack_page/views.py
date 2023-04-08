c7fc138b8508b0b349882b033428e43bf564b33b
alkokovad <59422453+alkokovad@users.noreply.github.com>
2023-04-08 16:04:14 +0300

    get the cards_list in template model

--- OO_hack_page/views.py
+++ OO_hack_page/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
 
+
+class CardsListView(View):
+    template_name = 'CardsList/cards_list.html'
+
+    def get(self, request):
+        return render(request, CardsListView.template_name)
