1c7883248aeba03fe53edb46ecd3889c285e13b8
X-xsiros <pasha.anfimov2@gmail.com>
2023-04-08 14:29:05 +0300

    тест

--- OO_hack_page/views.py
+++ OO_hack_page/views.py
@@ -1,31 +1,39 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterForm
 
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'users/register.html', { 'form': form})
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
+def sign_in(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('posts')
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+        return render(request, 'users/login.html', {'form': form})
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, f'Hi {username.title()}, welcome back!')
+                return redirect('posts')
+
+        # form is not valid or user is not authenticated
+        messages.error(request, f'Invalid username or password')
+        return render(request, 'users/login.html', {'form': form})
 
 
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+def sign_out(request):
+    logout(request)
+    messages.success(request,f'You have been logged out.')
+    return redirect('login')
