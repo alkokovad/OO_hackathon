32d6373651ef5206cda3c91c408a0331dd771123
alkokovad <59422453+alkokovad@users.noreply.github.com>
2023-04-08 14:17:45 +0300

    part of CRUD for cards

--- OO_hack/settings.py
+++ OO_hack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ahyfu8h7=d)(%0z*@6vcp__!r_l74x^#*dez+=y3kyv0w)txt$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OO_hack_page.apps.OoHackPageConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'OO_hack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'Moscow/Europe'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -119,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "Static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
